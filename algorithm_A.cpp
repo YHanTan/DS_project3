bce51f91a23f3edfcb29421a1706c89b6c9fe9fd
YHanTan <56245065+YHanTan@users.noreply.github.com>
2020-01-15 19:13:30 +0800

    avoid "null"_v2

--- algorithm_A.cpp
+++ algorithm_A.cpp
@@ -40,12 +40,6 @@ void algorithm_A(Board board, Player player, int index[]){
     int row_tmp, col_tmp;
     int color = player.get_color();
 
-    while(1){
-        row_tmp = rand() % 5;
-        col_tmp = rand() % 6;
-        if(board.get_cell_color(row_tmp, col_tmp) == color || board.get_cell_color(row_tmp, col_tmp) == 'w') break;
-    }
-
     if(board.get_cell_color(0, 0) == 'w'){
         index[0] = 0;
         index[1] = 0;
@@ -79,6 +73,14 @@ void algorithm_A(Board board, Player player, int index[]){
         index[1] = 5;
     }
     else{
+        //avoid "null"
+        while(1){
+            row = rand() % 5;
+            col = rand() % 6;
+            if(board.get_cell_color(row, col) == color || board.get_cell_color(row, col) == 'w') break;
+        }
+
+        //put "orbs" around diagonal
         for(int k=0;k<ROW;k++){
             for(int i=0, j=k;i<=k &&j>=0;i++, j--){
                 if(board.get_cell_color(i,j)==color || board.get_cell_color(i,j)=='w'){
